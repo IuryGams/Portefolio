d7e784fe9e5b158276fd369900e8601857070056
Iury Gama <iuryempregos@gmail.com>
2023-11-14 20:53:57 -0300

    create GlobalStyle

---
+++ src/theme/GlobalStyle.jsx
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    *{
+        padding: 0;
+        margin: 0;
+        box-sizing: border-box;
+        font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+        text-decoration: none;
+        list-style: none;
+    }
+
+    body{
+        background-color: #E9e9e9;
+    }
+`
+
+export default GlobalStyle;
